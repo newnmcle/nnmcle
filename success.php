073e364aebcbfea628cb75a440b0ca4fad11ca4e
newnmcle <86611249+newnmcle@users.noreply.github.com>
2021-06-29 19:45:30 +0545

    Update success.php

--- success.php
+++ success.php
@@ -1,29 +1,5 @@
 <?php 
 
 echo "<h1> success";
-
-$pid = $_GET['oid'];
-$red = $_GET['refId'];
-
-
-
-
- 
-$data =[
-    'amt'=> 1000,
-    'rid'=> $red,
-    'pid'=> $pid,
-    'scd'=> 'EPAYTEST'
-];
-
-    $curl = curl_init("https://uat.esewa.com.np/epay/transrec");
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl);
-    echo $response;
-    curl_close($curl);
     
-    
-
-?>
+ ?>
